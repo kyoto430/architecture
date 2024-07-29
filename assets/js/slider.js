699757e3e45fb0e7b3d6fbeb839f0131bd9a544a
Kyoto430 <nateriver345@gmail.com>
2024-07-29 16:41:09 +0300

    feat: add adaptive

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,8 +1,7 @@
-if (window.innerWidth <= 768) {
-  console.log('slider running...');
+if (window.innerWidth <= 1024) {
+  console.log('slider-1 running...');
 
   new Swiper('.slider-1', {
-    spaceBetween: 20,
     loop: false,
     navigation: {
       prevEl: '.btn-prev-1',
@@ -10,6 +9,174 @@ if (window.innerWidth <= 768) {
     },
     pagination: {
       el: '.pagination-1',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-2', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-2',
+      nextEl: '.btn-next-2',
+    },
+    pagination: {
+      el: '.pagination-2',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-3', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-3',
+      nextEl: '.btn-next-3',
+    },
+    pagination: {
+      el: '.pagination-3',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-4', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-4',
+      nextEl: '.btn-next-4',
+    },
+    pagination: {
+      el: '.pagination-4',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-5', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-5',
+      nextEl: '.btn-next-5',
+    },
+    pagination: {
+      el: '.pagination-5',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-6', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-6',
+      nextEl: '.btn-next-6',
+    },
+    pagination: {
+      el: '.pagination-6',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-7', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-7',
+      nextEl: '.btn-next-7',
+    },
+    pagination: {
+      el: '.pagination-7',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-8', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-8',
+      nextEl: '.btn-next-8',
+    },
+    pagination: {
+      el: '.pagination-8',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-9', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-9',
+      nextEl: '.btn-next-9',
+    },
+    pagination: {
+      el: '.pagination-9',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-10', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-10',
+      nextEl: '.btn-next-10',
+    },
+    pagination: {
+      el: '.pagination-10',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-11', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-11',
+      nextEl: '.btn-next-11',
+    },
+    pagination: {
+      el: '.pagination-11',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-12', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-12',
+      nextEl: '.btn-next-12',
+    },
+    pagination: {
+      el: '.pagination-12',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-13', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-13',
+      nextEl: '.btn-next-13',
+    },
+    pagination: {
+      el: '.pagination-13',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-14', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-14',
+      nextEl: '.btn-next-14',
+    },
+    pagination: {
+      el: '.pagination-14',
+      clickable: true,
+    },
+  });
+
+  new Swiper('.slider-15', {
+    loop: false,
+    navigation: {
+      prevEl: '.btn-prev-15',
+      nextEl: '.btn-next-15',
+    },
+    pagination: {
+      el: '.pagination-15',
       clickable: true,
     },
   });
